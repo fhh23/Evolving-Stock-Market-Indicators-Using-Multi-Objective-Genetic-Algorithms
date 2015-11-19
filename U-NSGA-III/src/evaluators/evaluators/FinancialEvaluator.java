7dfd64ef2492fef871d900c7c2f43e49dfbc787a
Bonnie R <bpr005@bucknell.edu>
2015-11-19 15:37:19 -0500

    Adding more to the Java skeleton based on the discussion with Haitham. The XML file needs to be updated to add variables and boundary constraints (the objective functions will be defined in the Java code).

--- U-NSGA-III/src/evaluators/evaluators/FinancialEvaluator.java
+++ U-NSGA-III/src/evaluators/evaluators/FinancialEvaluator.java
@@ -32,10 +32,9 @@ public class FinancialEvaluator extends IndividualEvaluator
     }
 
     @Override
-    /* TODO: override this appropriately */
 	public VirtualIndividual[] getParetoFront(int objectivesCount, int n) throws InvalidOptimizationProblemException, XMLStreamException, EvaluationException 
     {
-        throw new UnsupportedOperationException("getParetoFront() method must be overrided with appropriate logic.");
+        throw new UnsupportedOperationException("getParetoFront() is not defined for this problem.");
     }
     
     public void updateIndividualObjectivesAndConstraints(
@@ -43,7 +42,43 @@ public class FinancialEvaluator extends IndividualEvaluator
             Individual individual)
             throws EvaluationException 
     {
-    	// TODO: see example code in ZDT1Evaluator.java
+        double[] x = individual.real;
+        double[] objs = getObjectives(calculateindicators(x));
+        individual.setObjective(0, objs[0]);
+        individual.setObjective(1, objs[1]);
+        // Announce that objective function values are valid
+        individual.validObjectiveFunctionsValues = true;
+        // Update constraint violations if constraints exist
+        if (problem.constraints != null) {
+            // Five non-boundary constraints   
+            double[] g = new double[5];
+            g[0] = 0;
+            g[1] = 0;
+            g[2] = 0; 
+            g[3] = 0;
+            g[4] = 0;
+            // Set constraints violations
+            for (int i = 0; i < g.length; i++) {
+                if (g[i] < 0) {
+                    individual.setConstraintViolation(i, g[i]);
+                } else {
+                    individual.setConstraintViolation(i, 0);
+                }
+            }
+            // Announce that objective function values are valid
+            individual.validConstraintsViolationValues = true;
+        }
+        // Increase Evaluations Count by One (counted per individual)
+        individualEvaluationsCount++;
+    }
+    
+    // TODO: calculate the value of  the four indicator functions using the values in the chromosome
+    double[] calculateindicators(double[] x) {
+    	throw new UnsupportedOperationException();
     }
 
+    // TODO: run the simulation using the new indicator values and output the value of the objective functions
+    double[] getObjectives(double[] indicators) {
+    	throw new UnsupportedOperationException();
+    }
 }
